0f5c60dc6c40976fc994bb357229ccfaa7d6a947
RishabhKumar <rishabhkumar.rk755@gmail.com>
2026-08-07 23:28:53 +0530

    Initial commit

---
+++ src/main/java/com/owner_service/TmsOwnerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.owner_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsOwnerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsOwnerServiceApplication.class, args);
+	}
+
+}
