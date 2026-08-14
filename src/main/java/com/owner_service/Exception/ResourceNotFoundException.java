6cd8a4cbedb07dd393d54f99bec9f4afff910225
RishabhKumar <rishabhkumar.rk755@gmail.com>
2026-08-15 05:08:34 +0530

    feat: Add update,delete api for owner service

---
+++ src/main/java/com/owner_service/Exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.owner_service.Exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
